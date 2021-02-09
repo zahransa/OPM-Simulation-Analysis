86dc2a4c46d09b4f8873d0c83aefcfc120c35af7
SaeedZahran <saeedzahran@hotmail.com>
2021-02-09 12:55:24 +0100

    add the meg channel

--- Steps.m
+++ Steps.m
@@ -1,3 +1,7 @@
 % STEPS:
 
-% 1. 
+% 1. Run Brainstorm
+% 2. Create a new protocol
+% 3. Import the subject anatomy (Go to the Download page of the brainstorm website, and download the file: sample_introduction.zip) -- Unzip it in a folder that is not in any of the Brainstorm folders (program folder or database folder).
+% 4. Switch to the "functional data" view -- Right-click on the subject folder > Review raw file -- Select the file format: "MEG/EEG: CTF (*.ds...)" -- Select all the .ds folders in: sample_introduction/data
+% 5. 
