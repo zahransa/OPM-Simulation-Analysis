8e80ae46f1eb8ca71477e5d036a5d8a462c2ceea
SaeedZahran <saeedzahran@hotmail.com>
2021-02-09 15:55:43 +0100

    Creat arrays from SQUID

--- Steps.m
+++ Steps.m
@@ -4,4 +4,10 @@
 % 2. Create a new protocol
 % 3. Import the subject anatomy (Go to the Download page of the brainstorm website, and download the file: sample_introduction.zip) -- Unzip it in a folder that is not in any of the Brainstorm folders (program folder or database folder).
 % 4. Switch to the "functional data" view -- Right-click on the subject folder > Review raw file -- Select the file format: "MEG/EEG: CTF (*.ds...)" -- Select all the .ds folders in: sample_introduction/data
-% 5. 
+% 5. Load the channelmeg.mat in matlab then right click on CTF channels ->
+% file -> import from matlab and choose channelmeg
+% 6. Run center_of_meg.m to get the center of each meg sensors
+% 7. Export the scalp to the matlab then do: scalpp=scalp.Vertices;
+% scalpnn=scalp.VertNormals; -- for ind = 1: 102 coiln(ind,:)=chmeg.Channel(ind).Orient(:,1); end
+% 8. Run CreateArraysFromSQUIDs.m (this script need ConstructRotationMatrix.m)
+% 9.  
