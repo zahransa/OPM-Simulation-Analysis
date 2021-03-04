a9dcb338293ca6bf850274b10ad22490a8743c1d
SaeedZahran <saeedzahran@hotmail.com>
2021-03-04 13:32:01 +0100

    tp

--- Primary- and volume-current/TP_PV.m
+++ Primary- and volume-current/TP_PV.m
@@ -1,41 +1,73 @@
-
 %T=P+V
 
-megv=megt.Gain - megp.Gain;
-opmnv=opmnt.Gain - opmnp.Gain;
-opmt1v=opmt1t.Gain - opmt1p.Gain;
-opmt2v=opmt2t.Gain - opmt2p.Gain;
 
 
-Tmegx=sqrt(sum(megt.Gain.^2, 1));
-Pmegx=sqrt(sum(megp.Gain.^2, 1));
-Vmegx=sqrt(sum(megv.^2, 1));
-TPmegx=Tmegx./Pmegx;
-meanTPmegx=mean(TPmegx);
-PVmegx=Pmegx./Vmegx;
-meanPVmegx=mean(PVmegx);
 
-Topmnx=sqrt(sum(opmnt.Gain.^2, 1));
-Popmnx=sqrt(sum(opmnp.Gain.^2, 1));
-Vopmnx=sqrt(sum(opmnv.^2, 1));
-TPopmnx=Topmnx./Popmnx;
-meanTPopmnx=mean(TPopmnx);
-PVopmnx=Popmnx./Vopmnx;
-meanPVopmnx=mean(PVopmnx);
+%%%%%%%%%%%%%%%%%
+megv=Gain_normal_squid - Gain_normal_squidp;
 
-Topmt1x=sqrt(sum(opmt1t.Gain.^2, 1));
-Popmt1x=sqrt(sum(opmt1p.Gain.^2, 1));
-Vopmt1x=sqrt(sum(opmt1v.^2, 1));
-TPopmt1x=Topmt1x./Popmt1x;
-meanTPopmt1x=mean(TPopmt1x);
-PVopmt1x=Popmt1x./Vopmt1x;
-meanPVopmt1x=mean(PVopmt1x);
+Tmeg=sqrt(sum(Gain_normal_squid.^2, 1));
+Pmeg=sqrt(sum(Gain_normal_squidp.^2, 1));
+Vmeg=sqrt(sum(megv.^2, 1));
 
-Topmt2x=sqrt(sum(opmt2t.Gain.^2, 1));
-Popmt2x=sqrt(sum(opmt2p.Gain.^2, 1));
-Vopmt2x=sqrt(sum(opmt2v.^2, 1));
-TPopmt2x=Topmt2x./Popmt2x;
-meanTPopmt2x=mean(TPopmt2x);
-PVopmt2x=Popmt2x./Vopmt2x;
-meanPVopmt2x=mean(PVopmt2x);
+TPmeg=Tmeg./Pmeg;
+meanTPmeg=mean(TPmeg);
+
+PVmeg=Pmeg./Vmeg;
+meanPVmeg=mean(PVmeg);
+
+%%%%%%%%%%%%%%%%%%%%
+opmnv=Gain_normal_opmn - Gain_normal_opmnp;
+
+Topmn=sqrt(sum(Gain_normal_opmn.^2, 1));
+Popmn=sqrt(sum( Gain_normal_opmnp.^2, 1));
+Vopmn=sqrt(sum(opmnv.^2, 1));
+TPopmn=Topmn./Popmn;
+meanTPopmn=mean(TPopmn);
+PVopmn=Popmn./Vopmn;
+meanPVopmn=mean(PVopmn);
+
+%%%%%%%%%%%%%%%%%%%%
+opmt1v=Gain_normal_opmt1 - Gain_normal_opmt1p;
+
+Topmt1=sqrt(sum(Gain_normal_opmt1.^2, 1));
+Popmt1=sqrt(sum(Gain_normal_opmt1p.^2, 1));
+Vopmt1=sqrt(sum(opmt1v.^2, 1));
+TPopmt1=Topmt1./Popmt1;
+meanTPopmt1x=mean(TPopmt1);
+PVopmt1=Popmt1./Vopmt1;
+meanPVopmt1=mean(PVopmt1);
+
+
+%%%%%%%%%%%%%%%%%%%
+opmt2v=Gain_normal_opmt2 - Gain_normal_opmt2p;
+
+Topmt2=sqrt(sum(Gain_normal_opmt2.^2, 1));
+Popmt2=sqrt(sum(Gain_normal_opmt2p.^2, 1));
+Vopmt2=sqrt(sum(opmt2v.^2, 1));
+TPopmt2=Topmt2./Popmt2;
+meanTPopmt2=mean(TPopmt2);
+PVopmt2=Popmt2./Vopmt2;
+meanPVopmt2=mean(PVopmt2);
+
+
+%%%%%%%%%%%%%%%%%%%
+opmtav=gaina - gainap;
+
+Topma=sqrt(sum(gaina.^2, 1));
+Popma=sqrt(sum(gainap.^2, 1));
+Vopma=sqrt(sum(opmtav.^2, 1));
+TPopma=Topma./Popma;
+meanTPopma=mean(TPopma);
+PVopma=Popma./Vopma;
+meanPVopma=mean(PVopma);
+
+
+
+
+
+
+
+
+
 
